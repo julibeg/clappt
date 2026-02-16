86ace59c7c011d41528c09628c7a21024a744d8a
Julian Libiseller-Egger <37619875+julibeg@users.noreply.github.com>
2026-02-16 01:56:38 +0100

    silly

--- hooks/add-hooks-to-settings-and-symlink.sh
+++ hooks/add-hooks-to-settings-and-symlink.sh
@@ -25,10 +25,11 @@ mv "$SETTINGS.tmp" "$SETTINGS"
 
 # symlink this dir to ~/.claude/hooks (using a relative path so that in `clappt` we
 # don't have to expose the actual host path when binding to the container)
-set -x
 RELATIVE_SCRIPT_DIR="$(realpath --relative-to="$HOME/.claude" "$SCRIPT_DIR")"
-ln -sf "$RELATIVE_SCRIPT_DIR" .
 
+pushd ~/.claude
+ln -sf "$RELATIVE_SCRIPT_DIR" .
+popd
 
 echo -n "Updated hooks in '$SETTINGS' and symlinked '$SCRIPT_DIR' to '~/.claude/hooks' "
 echo "(using the relative path '$RELATIVE_SCRIPT_DIR')"
